8e013a4e76d460a605ea797c8a6957d8cddc1e65
Camila <camila@Matthews-MacBook-Pro-2.local>
2021-03-10 19:07:22 -0600

    .

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,19 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank
+    @Size(min=3)
     private String username;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
+    @Size(min=8, message = "Password must be at least 8 characters long ")
     private String password;
 
     public User() {
